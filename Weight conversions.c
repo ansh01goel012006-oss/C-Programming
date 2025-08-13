1c153a39644bdb747843b55d471202141f4f59b1
Ansh Goel <ansh01goel012006@gmail.com>
2025-08-13 12:29:30 +0530

    Add files via upload

---
+++ Weight conversions.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+float kg,gm;
+
+printf("grams:");
+scanf("%f", &gm);
+
+  kg=gm/1000;
+  printf("when grams are converted to killograms:%f \n",kg);
+
+
+
+
+
+printf("kilograms:");
+scanf("%f", &kg);
+
+gm=kg*1000;
+printf("when killograms are converted to grams:%f \n",gm);
+
+
+
+}
