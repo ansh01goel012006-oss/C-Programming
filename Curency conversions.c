2771b213991684782e66f2cf8565198532a108cd
Ansh Goel <ansh01goel012006@gmail.com>
2025-08-13 12:16:38 +0530

    Add files via upload

--- Curency conversions.c
+++ Curency conversions.c
@@ -7,7 +7,9 @@ printf("dollars:");
 scanf("%f", &dollars);
 
 rupees=dollars*48;
+pounds=rupees/70;
 printf("when dollars are converted to Rs:%f \n",rupees);
+printf("when dollars are converted to pounds:%f \n",pounds);
 
 
 
@@ -17,7 +19,7 @@ printf("rupees:");
 scanf("%f", &rupees);
 
 dollars=rupees/60;
-printf("when Rs are converted to $:%f \n",rupees);
+printf("when Rs are converted to $:%f \n",dollars);
 
 
 
