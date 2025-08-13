5f50e3fea370bbdce614a57921972c15411bd21c
Ansh Goel <ansh01goel012006@gmail.com>
2025-08-13 11:57:26 +0530

    Add files via upload

--- Time conversions.c
+++ Time conversions.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
 
 int main() {
-int hr,min;
+float hr,min;
 
 printf("Hours:");
-scanf("%d", &hr);
+scanf("%f", &hr);
 
 min=hr*60;
-printf("when hr is converted to min:%d/n",min);
+printf("when hr is converted to min:%f/n",min);
 
 
 
 
 
 printf("minutes:");
-scanf("%d", &min);
+scanf("%f", &min);
 
 hr=hr/60;
-printf("when minutes are converted to hours:%d/n",hr);
+printf("when minutes are converted to hours:%f/n",hr);
 
 
 }
+
+
+
+
+
