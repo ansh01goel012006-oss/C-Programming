ba5b414a90c3b78385a43f290664adc2bb8d89af
Ansh Goel <ansh01goel012006@gmail.com>
2025-08-13 11:12:52 +0530

    Add files via upload

---
+++ sumANDdiff.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+  int num1,num2, sum, diff,multiplication;
+
+  printf("Enter the first number:");
+  scanf("%d" , &num1);
+
+  printf("Enter the second number:");
+  scanf("%d" , &num2);
+
+  sum= num1+num2;
+
+  printf("this is sum: %d/n", sum);
+
+  diff=num1-num2;
+
+  printf("this is diff: %d/n", diff);
+
+  multiplication=num1*num2;
+
+  printf("this is multiplication:%d/n", multiplication);
+
+}
