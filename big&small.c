b4bcb39c74f83342f9687d91c2f1c9b214d045db
Ansh Goel <ansh01goel012006@gmail.com>
2025-08-20 12:02:10 +0530

    Add files via upload

---
+++ big&small.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+float main()
+{
+float num1, num2, num3;
+printf("Enter The First Number:");
+scanf("%f", &num1);
+printf("Enter the Second Number:");
+scanf("%f", &num2);
+printf("Enter the Third Number:");
+scanf("%f", &num3);
+if(num1>num2)
+{
+    if(num1>num3)
+        printf("The Largest Number is:%f\n",num1);
+    else
+         printf("The Largest Number is:%f\n",num3);
+}
+else
+{
+    if(num2>num3)
+        printf("The Largest Number is:%f\n",num2);
+    else
+        printf("The Largest Number is:%f\n",num3);
+}
+}
