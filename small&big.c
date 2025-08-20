6a7001c46d41fb00590d8a83b30fc3a38dfac103
Ansh Goel <ansh01goel012006@gmail.com>
2025-08-20 12:02:36 +0530

    Add files via upload

---
+++ small&big.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+float main()
+{
+float num1, num2;
+printf("Enter The First Number:");
+scanf("%f", &num1);
+printf("Enter the Second Number:");
+scanf("%f", &num2);
+if(num1>num2)
+{
+printf("The Largest Number is:%f\n",num1);
+printf("The Smallest Number is:%f\n",num2);
+}
+else
+{
+printf("The Largest Number is:%f\n",num2);
+printf("The Smallest Number is:%f\n",num1);
+}
+}
